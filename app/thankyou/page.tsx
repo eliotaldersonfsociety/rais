13d3b18382b8d27197bc1f7688f1fbbf6187d991
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-05-19 18:26:16 -0500

    Delete app/thankyou/page.tsx

--- app/thankyou/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import OrderConfirmation from "@/components/OrderConfirmation";
-
-export default function ThankYouPage() {
-  return <OrderConfirmation />;
-}
-
-export const dynamic = "force-dynamic";
