1fcefacddc82f7db083add08e7ee5e14703dd21e
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-05-20 00:20:30 -0500

    Update layout.tsx

--- app/panel/layout.tsx
+++ app/panel/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import "../globals.css"
 import "./dashboard-variables.css"
 
-export default function DashboardLayout({
+export default function DashboardLayouts({
   children,
 }: {
   children: React.ReactNode
