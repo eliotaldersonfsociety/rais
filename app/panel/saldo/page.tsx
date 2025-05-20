cb443306d803c488e56d35cafbc19acb44919e59
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-05-20 00:30:40 -0500

    Update page.tsx

--- app/panel/saldo/page.tsx
+++ app/panel/saldo/page.tsx
@@ -1,13 +1,13 @@
 import { UserBalanceTable } from "@/components/user-balance-table"
-import { DashboardLayout } from "@/components/dashboard-layouts"    
+import { DashboardLayouts } from "@/components/dashboard-layouts"    
 
 export default function UsersPage() {
   return (
-    <DashboardLayout>
+    <DashboardLayouts>
       <div className="container py-10">
         <h1 className="text-3xl font-bold mb-6">Gestión de Usuarios</h1>
         <UserBalanceTable />
       </div>
-    </DashboardLayout>
+    </DashboardLayouts>
   )
 }
