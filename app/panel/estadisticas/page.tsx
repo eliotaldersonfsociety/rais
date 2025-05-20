bf8161808b24be5e5c90ee3e547f93674d641c82
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-05-20 00:29:15 -0500

    Update page.tsx

--- app/panel/estadisticas/page.tsx
+++ app/panel/estadisticas/page.tsx
@@ -1,13 +1,13 @@
 import WebAnalytics from "@/components/web-analytics"
-import { DashboardLayout } from "@/components/dashboard-layouts"
+import { DashboardLayouts } from "@/components/dashboard-layouts"
 
 export default function Home() {
   return (
-    <DashboardLayout>
+    <DashboardLayouts>
     <main className="container mx-auto py-8 px-4">
       <h1 className="text-3xl font-bold mb-8">Panel de Análisis Web</h1>
       <WebAnalytics />
     </main>
-    </DashboardLayout>
+    </DashboardLayouts>
   )
 }
