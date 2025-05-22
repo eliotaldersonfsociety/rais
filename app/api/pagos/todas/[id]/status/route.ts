20552042b678e5e786c8379d2e51c11d952af050
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-05-22 18:28:10 -0500

    Update route.ts

--- app/api/pagos/todas/[id]/status/route.ts
+++ app/api/pagos/todas/[id]/status/route.ts
@@ -1,21 +1,27 @@
 // app/api/pagos/todas/[id]/status/route.ts
 import { db } from '@/lib/db';
 import { orders } from '@/lib/payu/schema';
+import { transactions } from '@/lib/transaction/schema';
 import { NextRequest, NextResponse } from 'next/server';
 import { eq } from 'drizzle-orm';
 
 export async function PATCH(req: NextRequest) {
-  const { status } = await req.json();
+  const { status, type } = await req.json();
 
   // Extrae el id de la URL
   const url = new URL(req.url);
   const pathParts = url.pathname.split('/');
   const id = pathParts[pathParts.length - 2]; // .../todas/[id]/status
 
-  // Solo actualiza en la tabla de PayU (payu_tab)
-  await db.transactions.update(orders)
-    .set({ status })
-    .where(eq(orders.referenceCode, id));
+  if (type === 'payu') {
+    await db.payu.update(orders)
+      .set({ status })
+      .where(eq(orders.referenceCode, id));
+  } else {
+    await db.transactions.update(transactions)
+      .set({ status })
+      .where(eq(transactions.id, Number(id)));
+  }
 
   return NextResponse.json({ ok: true });
 }
