5daf2310238c44e24fcb24e637a56c6907094b2f
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-05-21 19:00:30 -0500

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,7 +5,13 @@ import { users } from "@/lib/register/schema";
 export async function GET() {
   try {
     const allUsers = await db.users.select().from(users);
-    return NextResponse.json(allUsers);
+    return new NextResponse(JSON.stringify(allUsers), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store, must-revalidate",
+      },
+    });
   } catch (error) {
     return NextResponse.json({ error: "Error al obtener usuarios" }, { status: 500 });
   }
