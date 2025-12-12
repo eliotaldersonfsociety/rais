ebe79a55e583130a9d395db7a41e95e9b0ffcadb
rennyardiladev <rennyardiladev@gmail.com>
2025-12-12 17:20:06 -0500

    Actualización completa del proyecto

---
+++ next.config.ts
@@ -0,0 +1,32 @@
+import type { NextConfig } from "next";
+
+// Eliminar la configuración para el análisis de bundles
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  compress: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "www.apple.com" },
+      { protocol: "https", hostname: "placehold.co" },
+    ],
+  },
+  modularizeImports: {
+    lodash: { transform: 'lodash/{{member}}' },
+    '@mui/icons-material': { transform: '@mui/icons-material/{{member}}' },
+  },
+  productionBrowserSourceMaps: false,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
